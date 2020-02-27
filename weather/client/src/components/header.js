b6befd18897021230437a4b0daa7492197c060ef
Yoonho Kim <Daniel@MiMIX2-MiPhone.localdomain>
2020-02-27 15:21:20 -0800

    client server separated

--- weather/client/src/components/header.js
+++ weather/client/src/components/header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function header() {
   return (
-    <div className="header">
+    <header className="header">
       <h1 className="header__title">WeatherMon</h1>
-    </div>
+    </header>
   );
 }
